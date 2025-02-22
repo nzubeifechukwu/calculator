a10af266e34f72cdd03b054a8c3d38f853b7d9a3
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2025-02-22 20:56:39 +0100

    Create add, subtract, multiply, divide, and operate functions

---
+++ index.js
@@ -0,0 +1,36 @@
+function operate(num1, operator, num2) {
+  if (operator === "+") {
+    return add(num1, num2);
+  }
+  if (operator === "-") {
+    return subtract(num1, num2);
+  }
+  if (operator === "*") {
+    return multiply(num1, num2);
+  }
+  if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+const num1 = 15;
+const num2 = 3;
+operator = "/";
+
+console.log(operate(num1, operator, num2));
