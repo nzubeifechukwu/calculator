37d548852e18c88b6e75e8b81695cc49c90cc1de
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2025-02-28 12:28:52 +0100

    Implement basic math operations

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const displayDiv = document.querySelector(".display");
-// displayDiv.textContent = "";
 let displayVal = "";
+let values = [];
+let operator;
 
 const digits = [
   "zero",
@@ -19,11 +20,34 @@ const digitBtns = digits.map((digit) => document.querySelector(`.${digit}`));
 
 digitBtns.map((btn) =>
   btn.addEventListener("click", (event) => {
-    // console.log(event);
     displayVal += event.target.textContent;
     displayDiv.textContent = displayVal;
   })
 );
+
+const operations = ["add", "subtract", "multiply", "divide"];
+
+const operationBtns = operations.map((operation) =>
+  document.querySelector(`.${operation}`)
+);
+
+operationBtns.map((btn) =>
+  btn.addEventListener("click", (event) => {
+    operator = event.target.textContent;
+    values.push(parseInt(displayVal));
+    displayVal = "";
+    // console.log(values);
+  })
+);
+
+const equalsBtn = document.querySelector(".equals");
+equalsBtn.addEventListener("click", () => {
+  values.push(parseInt(displayVal));
+  displayVal = operate(values[0], operator, values[1]);
+  console.log(displayVal);
+  displayDiv.textContent = displayVal;
+  displayVal = "";
+});
 
 function operate(num1, operator, num2) {
   if (operator === "+") {
